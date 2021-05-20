f3906f0752d95ff2023737061a87ef9bc942eb9e
RaahiKadu <80748411+RaahiKadu@users.noreply.github.com>
2021-05-20 15:38:13 +0530

    Create BT_traversal.java

--- BT_traversal.java
+++ BT_traversal.java
@@ -1,242 +1,310 @@
-// BT WITH AND WITHOUT RECURRSION 
-package norecursion;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
-import  java.util.Stack;
+import java.util.Stack; 
 
 
-//import tree.Node;
+class node{
+    node l,r;
+    int data;
+    
+    public node(int d){
+        l=null;
+        r=null;
+        data=d;
+    }
+}
 
-class Node //Node class
-{
-	int data;
-	Node left;
-	Node right;
-	public Node(int data) //constructor
-	{
-		this.data = data;
-		right = null;
-		left = null;
+class bt{
+    node root;
+    Scanner sc=new Scanner(System.in);
+    
+    public bt(){
+        root=null;
+    }
+
+    void create(){
+        int d;
+        char dir,ch;
+        bt tree = new bt();
+        do{
+            // accept data
+            System.out.print("Enter data : ");
+            d = sc.nextInt();
+            //create node
+            node temp= new node(d);
+            
+            //btree is empty
+            if(root==null){
+                root = temp;
+                System.out.println("Root node added!");
+            }
+            //btree isn't empty,a root node exists
+            else{
+                node ptr;
+                ptr = root;
+                while (ptr != null){
+                    System.out.println("Current node Data : " + ptr.data);
+                    // accept direction to insert Node
+                    System.out.print("Which direction of node with data '" + ptr.data + "' to add the node(l/r)? : ");
+                    dir = sc.next().charAt(0);
+                    if (dir == 'L' || dir == 'l'){
+                        // if l=null insert new node there, else add ptr at l
+                        if (ptr.l == null){
+                            ptr.l = temp;
+                            System.out.println("Node added!!");
+                            break;
+                        }
+                        else{
+                            ptr = ptr.l;
+                        }
+                    }
+                    else{ //dir==r
+                        // if r=null insert newl node there, else add ptr at r
+                        if (ptr.r == null){
+                            ptr.r = temp;
+                            System.out.println("Node added!!");
+                            break;
+                        }
+                        else{
+                            ptr = ptr.r;
+                        }
+                    }
+                    
+                }
+            }
+            System.out.print("\nDo you want to add more nodes ?(y/n) : ");
+            ch = sc.next().charAt(0);
+            System.out.println("");
+        }while (ch != 'N' && ch != 'n');
+    }//create ends
+
+    //Inorder Recursive
+    void inOrderRecursive(node localRoot){
+        if (localRoot != null){
+            inOrderRecursive(localRoot.l);
+            System.out.print(localRoot.data + " ");
+            inOrderRecursive(localRoot.r);
+        } 
+    }
+    
+    //Preorder Recursive
+    void preOrderRecursive(node localRoot){
+        if (localRoot != null){
+            System.out.print(localRoot.data + " ");
+            preOrderRecursive(localRoot.l);
+            preOrderRecursive(localRoot.r);
+        }
+    }
+    
+    //Postorder Recursive
+    void postOrderRecursive(node localRoot){
+        if (localRoot != null){
+            postOrderRecursive(localRoot.l);
+            postOrderRecursive(localRoot.r);
+            System.out.print(localRoot.data + " ");
+        }
+    }
+    
+    //Inorder Non-recursive
+    void inOrderNonRecursive(){
+        Stack<node> s = new Stack<node>();
+        node ptr = root;
+        node popped;
+        if (root == null){
+            System.out.println("Empty tree!!");
+            return;
+        }
+        while (true){
+            while (ptr != null){
+                s.push(ptr);
+                ptr = ptr.l;
+            }
+            if (!s.empty()){
+                popped = s.pop();
+                ptr = popped;
+                System.out.print(popped.data + " ");
+                ptr = ptr.r;
+            }
+            else
+                break;
+        }
+    } 
+
+    //Preorder Non-recursive
+    void preOrderNonRecursive(){
+        Stack<node> s = new Stack<node>();
+        node ptr = root; 
+        node popped;
+        if (root == null){
+            System.out.println("Empty tree!!");
+            return;
+        }
+        while (true){
+            while (ptr != null){
+                System.out.print(ptr.data + " ");
+                s.push(ptr);
+                ptr = ptr.l;
+            }
+            if (!s.empty()){
+                popped =s.pop();
+                ptr = popped;
+                ptr = ptr.r;
+            }
+            else
+                break;
+        }
+    }
+    
+    //Postorder Non-recursive
+    void postOrderNonRecursive(){
+        Stack<node> s1 = new Stack<node>();
+        Stack<Character> s2 = new Stack<>();
+        node ptr = root;
+        node popped;
+        char poppedFlag;
+        if (root == null){
+            System.out.println("Tree is Empty!!");
+            return;
+        }
+        while (true){
+            while (ptr != null){
+                s1.push(ptr);
+                s2.push('L');
+                ptr = ptr.l;
+            }
+            if (!s1.empty()){
+                popped = s1.pop();
+                ptr = popped;
+                poppedFlag = s2.pop();
+                if (poppedFlag == 'L'){
+                    s1.push(ptr); 
+                    s2.push('R');
+                    ptr = ptr.r;
+                }
+                else{
+                    System.out.print(ptr.data + " ");
+                    ptr = null;
+                }
+            }
+            else
+                break;
+        }
+    }
+
+    //Level-order
+    void levelOrder(node localRoot){
+        Queue<node> q = new LinkedList<>();
+        node ptr = root;
+        if (root == null){
+            System.out.println("Tree Empty!!");
+            return;
+        }
+        while (ptr != null){
+            System.out.print(ptr.data + " ");
+            if (ptr.l!= null)
+                q.add(ptr.l);
+                
+            if (ptr.r!= null)
+                q.add(ptr.r);
+                
+            if (q.isEmpty())
+                ptr = null;
+                
+            else
+                ptr = q.remove();
+        }
+    }
+}//btree ends
+
+
+public class Main{
+	public static void main(String[] args) {
+	    int op;
+	    bt b=new bt();
+	    Scanner sc=new Scanner(System.in);
+	    do{
+            System.out.println("**************MENU*********************");;
+            System.out.println("1.Create a Binary Tree");
+            System.out.println("Display using:");
+            System.out.println("  2.Recursive Inorder");
+            System.out.println("  3.Recursive Preorder");
+            System.out.println("  4.Recursive Postorder");
+            System.out.println("  5.Non-recursive Inorder");
+            System.out.println("  6.Non-recursive Preorder");
+            System.out.println("  7.Non-recursive Postorder");
+            System.out.println("  8.Level order");
+            System.out.println("9.Exit");
+            System.out.println("*************************************");
+            System.out.print("Enter your choice: ");
+            op=sc.nextInt();
+            switch(op){
+                case 1:
+                    b.create();
+                    break;
+                    
+                case 2:
+                    System.out.println("\nThe Recursive Inorder is: ");
+                    if (b.root == null)
+                        System.out.print("Tree Empty!!");
+                    else
+                        b.inOrderRecursive(b.root);
+                    System.out.println("");
+                    break;
+                    
+                case 3:
+                    System.out.println("\nThe Recursive Preorder is: ");
+                    if (b.root == null)
+                        System.out.print("Tree Empty!!");
+                    else
+                        b.preOrderRecursive(b.root);
+                    System.out.println("");
+                    break;
+
+                case 4:
+                    System.out.println("\nThe Recursive Postorder is: ");
+                    if (b.root == null)
+                        System.out.print("Tree Empty!!");
+                    else
+                        b.postOrderRecursive(b.root);
+                    System.out.println("\n");
+                    break;
+    
+                case 5:
+                    System.out.println("\nThe Non-recursive Inorder is: ");
+                    b.inOrderNonRecursive();
+                    System.out.println(""); 
+                    break;
+                    
+                case 6:
+                    System.out.println("\nThe Non-recursive Preorder is: ");
+                    b.preOrderNonRecursive();
+                    System.out.println("");
+                    break;
+            
+                case 7:
+                    System.out.println("\nThe Non-recursive Postorder is: ");
+                    b.postOrderNonRecursive();
+                    System.out.println("");
+                    break;
+                 
+                case 8:
+                     System.out.println("\nThe Level order is: ");
+                    if (b.root == null)
+                        System.out.print("Tree Empty!!");
+                    else
+                        b.levelOrder(b.root);
+                    System.out.println("");
+                    break;
+
+                case 9:
+                    System.out.println("Exited");
+                    break;
+                    
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        }while (op!=9);
 	}
-}
-
-class Binaryoperations {
-       
-
-	 Node root = null; //root node
-	Scanner scan = new Scanner(System.in);
-	public void create()
-	{
-	int data;
-	Node ptr,temp;
-	String choice ,dir;
-
-		do {
-			System.out.println("ENTER THE DATA:");
-			data = scan.nextInt();
-			scan.nextLine();
-		    temp = new Node(data);
-			
-			//temp.data = data;
-			//temp.right = temp.left = null;
-			if(root == null) //check if root is null
-			{
-				root = temp;
-			}
-			else
-			{
-				ptr = root;
-				while(ptr!= null)
-				{
-					System.out.println(ptr.data +" ");
-					System.out.println("ENTER THE DIRECTION:(left/right)");
-					dir = scan.nextLine();
-					if(dir.equalsIgnoreCase("left")) //go to left 
-					{
-						if(ptr.left == null)
-						{
-							ptr.left = temp;
-							break;
-						}
-						else
-						{
-							ptr = ptr.left;
-						}
-					}
-					else 
-					{
-						if(ptr.right == null) //go to right 
-						{
-							ptr.right = temp;
-							break;
-						}
-						else
-						{
-							ptr = ptr.right;
-						}
-					}
-				}
-			}System.out.println("DO YOU WANT TO CONTINUE ? 1.YES(y) 2.NO(n)");
-			choice = scan.nextLine();
-			}while(choice.equals("y") || choice.equals("Y"));
-
-        }
-        
-        public void inorder(Node node) //non recursive inorder
-        {
-        	Stack<Node> s = new Stack<Node>(); //stack s
-        	Node ptr = root;
-        	do
-        	{
-        	while(ptr != null) {
-        		
-        		s.push(ptr);
-        		ptr = ptr.left;
-        	}
-        	if(!s.empty())
-        	{
-        		ptr = s.pop();
-        		System.out.println(ptr.data +" ");
-        		ptr= ptr.right;
-        	}
-        }while(!s.empty() || ptr!=null);
-}
-        	
-        public void preorder(Node node) //non recursive preorder
-        {
-        	Stack<Node> s = new Stack<Node>(); //stack s
-        	Node ptr = root;
-        	do
-        	{
-        		while(ptr != null)
-        		{
-        			System.out.println(ptr.data +" ");
-        			s.push(ptr);
-        			ptr = ptr.left;
-        		}
-        		if(!s.empty())
-        		{
-        			ptr = s.pop();
-        			ptr = ptr.right;
-        			
-        		}
-        	}while(!s.empty() || ptr!=null);
-        	
-        }
-
-        
-        public void postorder(Node node) //non recursive postorder
-        {
-        	Stack<Node> s1 = new Stack<Node>(); 
-        	Stack<Character> s2 = new Stack<Character>();
-        	Node ptr = root;
-        	do
-        	{
-        		while(ptr != null)
-        		{
-     
-        				s1.push(ptr);
-        				s2.push('L');
-        				ptr = ptr.left;
-        		
-        		}
-                 ptr = s1.pop();
-        			if(s2.pop() == 'L')
-        			{
-        				s1.push(ptr);
-        				s2.push('R');
-        				ptr = ptr.right;
-        			}
-        			else
-            			{
-            				System.out.println(ptr.data +" ");
-            				ptr = null;
-            			}
-        				
-        			
-       }while(!s1.empty() || ptr!=null || !s2.empty());
-        	
-        }
-        public void Preorder(Node node)//recursive preorder
-    	{
-    		if(node != null) {
-    		
-    			System.out.println(node.data +" ");
-    			preorder(node.left);
-    			preorder(node.right);
-    		}
-    			
-    	}
-    	
-    	public void Inorder(Node node)//recursive inorder
-    	{
-    		if(node!= null)
-    		{
-    			
-    			inorder(node.left);
-    			System.out.println(node.data +" ");
-    			inorder(node.right);
-    		}
-    	}
-    	
-    	public void Postorder(Node node)//recursive postorder
-    	{
-    		if(node !=null)
-    	{
-    			
-    		postorder(node.left);
-    		postorder(node.right);
-    		System.out.println(node.data +" ");
-    	}
-    	}
-        public static void main(String[] args) { //main class
-    		// TODO Auto-generated method stub
-        	
-        	int choice , ch;
-        	Scanner scan = new Scanner(System.in);
-        	Binaryoperations t1 = new Binaryoperations();
-        	do
-        	{
-        		System.out.println("******MENU******");
-            	System.out.println("1.CREATION OF TREE");
-            	System.out.println("2.INORDER TRAVERSAL");
-            	System.out.println("3.PREORDER TRAVERSAL");
-            	System.out.println("4.POSTORDER TRAVERSAL");
-            	System.out.println("5.EXIT");
-            	System.out.println("ENTER YOUR CHOICE:");
-            	choice = scan.nextInt();
-        	switch(choice)
-        	{
-        	
-        	case 1: 
-        		   t1.create(); //call create 
-        	        break;
-        	case 2: 
-        		   System.out.println("(NON RECURSION) INORDER IS :");
-     		       t1.inorder(t1.root);
-     		       System.out.println("(RECURSION) INORDER IS :");
-     		       t1.inorder(t1.root);
-     	            break;
-        	case 3:
-        		   System.out.println("(NON RECURSION) PREORDER IS :");
-    		       t1.preorder(t1.root);
-    		       System.out.println("(RECURSION) PREORDER IS :");
-    		       t1.preorder(t1.root);
-    	           break;
-        	case 4:
-        		   System.out.println("(NON RECURSION) POSTORDER IS :");
-    		       t1.postorder(t1.root);
-    		       System.out.println("(RECURSION) POSTORDER IS :");
-    		       t1.postorder(t1.root);
-    	           break;
-            case 5:	
-        		   System.out.println("EXIT");
-        	        break;
-        	        
-        	 default : System.out.println("INVALID CHOICE");
-        	           break;
-        	}
-        	   System.out.println("DO YOU WANT TO CONTINUE(menu) ? 1(continue) OR 0(stop) ");
-        	   ch = scan.nextInt();
-        	}while(ch !=0);
-        }
 }
